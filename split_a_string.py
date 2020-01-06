976bf0c2242c6dc5944b7acf99a3caf09a175f74
Tegan Broderick <teganbroderick@gmail.com>
2020-01-05 22:04:28 -0600

    WIP

--- split_a_string.py
+++ split_a_string.py
@@ -2,3 +2,15 @@
 
 def split(astring, splitter):
     """Split a string by splitter and return list of splits."""
+
+    word_list = []
+    for char in astring:
+        #iterate through chars,
+        for char in astring:
+        #concatentate chars until we reach the split word
+        
+        #append concatenated word
+        
+        #skip over split word
+        
+        #start process again
