8eaee0fcb7a4a23de3b7575aa66dcd0068cd7e2e
Tegan Broderick <teganbroderick@gmail.com>
2020-01-11 17:03:15 -0800

    Initial file with pseudocode and class skeletons

---
+++ queues_and_stacks.py
@@ -0,0 +1,21 @@
+#general code for queues and stacks
+
+class queue():
+
+    def __init__(self):
+
+    #FIFO
+    #enqueue(item)
+    #dequeue()
+    #peek() #see what is at the front of the queue
+    #isempty()
+
+class stack():
+
+    def __init__(self):
+
+    #LIFO
+    #push(item)
+    #pop()
+    #peek() - see what is at the top of the stack
+    #is_empty()
