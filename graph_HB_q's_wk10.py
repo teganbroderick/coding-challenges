fc11aefb87069c08824140815fd8fa0d98f4a554
Tegan Broderick <teganbroderick@gmail.com>
2020-01-14 16:14:28 -0800

    repeated challenge 3, to see if i could remember how to do it

--- graph_HB_q's_wk10.py
+++ graph_HB_q's_wk10.py
@@ -45,6 +45,7 @@ def connect_nodes(node):
 """
 Write a function that takes in two Node objects and returns True if the nodes are connected to one another.
 """
+#jan 13th
 
 def are_nodes_adjacent(node1, node2):
     """return True if nodes are adjacent to one another"""
@@ -62,6 +63,61 @@ def are_nodes_adjacent(node1, node2):
 
     if node2 in seen_set:
         return True
+
+#jan 14th from memory
+def are_nodes_connected(node1, node2):
+    """return true if nodes are connected to one another"""
+
+    seen_set = set()
+    nodes_to_check = [node1]
+
+    while nodes_to_check != []:
+        current = nodes_to_check.pop()
+
+        adj_nodes = current.adjacent()
+        for adj_node in adj_nodes:
+            if adj_nodes = node2:
+                return True
+            else:
+                seen_set.add(adj_node)
+                nodes_to_check.append(adj_node)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
