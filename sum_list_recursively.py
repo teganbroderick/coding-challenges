78a7f7bbdaf9911c10dea5550efca0c2309cd510
Tegan Broderick <teganbroderick@gmail.com>
2020-01-25 16:57:36 -0800

    Initial file, tested, solution works

--- sum_list_recursively.py
+++ sum_list_recursively.py
@@ -1,17 +1,3 @@
-#sum a list using recursion
-
-# def sum_list(nums):
-#     """Using recursion, return the sum of numbers in a list."""
-
-
-#     if nums == []:
-#         return 0
-    
-#     return nums[0] + sum_list(nums[1:])
-
-# print(sum_list([-5, 10, 4]))
-
-
 def sum_list_recursively(list1):
     """return sum of list items using recursion"""
 
