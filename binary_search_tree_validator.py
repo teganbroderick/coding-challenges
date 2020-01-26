edc9c19ce393588199075e7bf87c8c84f2f345e3
Tegan Broderick <teganbroderick@gmail.com>
2020-01-25 16:51:46 -0800

    Attempt using recursion

--- binary_search_tree_validator.py
+++ binary_search_tree_validator.py
@@ -8,22 +8,18 @@ class Node:
         self.left = left
         self.right = right
         
-    def is_valid(self):
+    def is_valid(self, upper_limit = float('inf'), lower_limit = -float('inf')):
         """Is this tree a valid BST?"""
-        print("in function", self.data)
-        #while there are nodes to traverse
-        if self.left != None or self.right != None:
-            #if left node data is less that root, and right node data is greater than root
-            if self.left.data < self.data and self.right.data > self.data:
-                #conditions satisfied, keep traversing tree
-                #run method on left node and right node
-                self.left.is_valid()
-                self.right.is_valid()
-            else:
-                #conditions not satisfied, return false
-                print("in else conditional", self.data)
-                return False
-        #tree traversed, conditions met, return true
+        
+        if self.data > upper_limit or self.data < lower_limit:
+            return False
+        
+        if self.left != None:
+            return Node.is_valid(self.left, upper_limit, self.data)
+
+        if self.right != None:
+            return Node.is_valid(self.right, self.data, lower_limit)
+
         return True
 
 t = Node(4, Node(2, Node(1), Node(3)), Node(6, Node(5), Node(7)))
