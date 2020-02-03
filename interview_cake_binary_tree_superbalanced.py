4882f2a90745e5d5f6e11d3ff50e705ed5aae781
Tegan Broderick <teganbroderick@gmail.com>
2020-02-03 13:11:20 -0800

    Refined function with an option to return False sooner

--- interview_cake_binary_tree_superbalanced.py
+++ interview_cake_binary_tree_superbalanced.py
@@ -1,28 +1,39 @@
-def is_binary_tree_super_balanced(root):
+def is_balanced(tree_root):
     """return true if difference between depth of leafnodes is 1 or less"""
-
-    #traverse tree, DFS
-    nodes_to_check = [(root, 0)]
-
+    # Determine if the tree is superbalanced
+    
+    #traverse tree, start at root node
+    #keep track of node and depth (tuple)
+    #record depths in a list or set
+    #record nodes to check in a stack
+    
+    #once we get to a leaf, node. left == None and node.right == None,
+        #put depth in depth list
+    #if depth list length ever goes over 2, return False
+    #if differences between depths in list are more than one, return False
+    
+    to_check = [(tree_root, 0)]
     depth_list = []
-
-    while to_check != []:
-        current, depth = nodes_to_check.pop()
-        #found leaf
-        if current.left == None and current.right == None:
+    
+    while to_check:
+        current, depth = to_check.pop()
+        
+        if current.right == None and current.left == None:
             if depth not in depth_list:
                 depth_list.append(depth)
-        #not a leaf
-        else:
-            if current.right != None:
-                nodes_to_check.append((current.right, depth+1))     
-            elif current.left != None:
-                nodes_to_check.append((current.left, depth+1))   
+        
+            if len(depth_list) > 2:
+                return False
+            elif len(depth_list) == 2:
+                if depth_list[1] - depth_list[0] > 1 or depth_list[0] - depth_list[1] > 1:
+                    return False
 
-    if depth_list > 2:
-        return False
-    elif depth_list[1] - depth_list[0] > 1:
-        return False
-    else:
-        return True
+        if current.left != None:
+            to_check.append((current.left, depth + 1))
+            
+        if current.right != None:
+            to_check.append((current.right, depth + 1))
+        
+
+    return True
 
